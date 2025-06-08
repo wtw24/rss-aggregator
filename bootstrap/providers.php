691f0f1d09d942317355623db86c5e4008a76a83
Wladimir Wendland <wtw24.com@gmail.com>
2025-06-08 12:52:08 +0200

    feat(telescope): integrate Laravel Telescope with configuration and migrations

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,4 +5,5 @@ declare(strict_types=1);
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\SecurityServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
